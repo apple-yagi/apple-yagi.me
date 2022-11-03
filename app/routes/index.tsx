175bcfaa973a8447da17efc8eb60e0e3f8c33c59
apple-yagi <applesryu@gmail.com>
2022-11-03 22:26:39 +0900

    Remove unnecessary api fetch

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,5 @@
-import { LoaderFunction, MetaFunction } from "@remix-run/cloudflare";
-import { Link, useLoaderData } from "@remix-run/react";
-import { MicroCMSListResponse } from "microcms-js-sdk";
-import { microcmsApi } from "~/api";
-import { Content } from "~/api/types";
+import { MetaFunction } from "@remix-run/cloudflare";
 import { Container } from "~/components/Container";
-import { GitHubIcon } from "~/components/icons/GitHubIcon";
-import { QiitaIcon } from "~/components/icons/QiitaIcon";
-import { TwitterIcon } from "~/components/icons/TwitterIcon";
-import { ZennIcon } from "~/components/icons/ZennIcon";
-import { LinkItem } from "~/components/LinkItem";
 import { SocialLinks } from "~/components/SocialLinks";
 
 export const meta: MetaFunction = () => ({
@@ -17,13 +8,7 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
-export const loader: LoaderFunction = async () => {
-  return await microcmsApi.blogs.$get({ query: { limit: 10 } });
-};
-
 export default function Index() {
-  const blogs = useLoaderData<MicroCMSListResponse<Content>>();
-
   return (
     <Container>
       <div className="mt-24" />

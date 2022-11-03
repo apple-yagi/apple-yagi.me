2efe401bfa177499d41069533dfa53d39178b4be
apple-yagi <applesryu@gmail.com>
2022-11-04 00:20:37 +0900

    Add description meta

--- app/root.tsx
+++ app/root.tsx
@@ -16,6 +16,7 @@ export function links() {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "apple-yagi",
+  description: "Personal website of apple-yagi.",
   viewport: "width=device-width,initial-scale=1",
 });
 

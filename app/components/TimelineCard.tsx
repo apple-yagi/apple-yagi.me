59c77e5544f2bb3907ac81ac1dcae2fcd76e91c1
apple-yagi <applesryu@gmail.com>
2022-11-21 00:39:24 +0900

    公開日の表示形式を修正

--- app/components/TimelineCard.tsx
+++ app/components/TimelineCard.tsx
@@ -57,10 +57,10 @@ export const TimelineCard = ({
     >
       <div className="p-4">
         <p className="font-medium text-sm sm:text-base">{title}</p>
-        <div className="flex pt-3 gap-2">
+        <div className="flex pt-3 gap-2 items-center">
           <TimelineImage kind={kind} />
           <span className="text-xs sm:text-sm">
-            {new Date(pubDate).toDateString()}
+            {new Date(pubDate).toLocaleDateString()}
           </span>
         </div>
       </div>

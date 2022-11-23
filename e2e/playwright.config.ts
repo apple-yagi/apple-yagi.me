9ab757841b0d2ebb0039fa30e1aaf56de5f1b49a
apple-yagi <applesryu@gmail.com>
2022-11-23 23:08:12 +0900

    Run e2e test after deploy

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,9 +1,12 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 
+const isProduction = process.env.NODE_ENV === "production";
+const baseURL = isProduction ? "https://apple-yagi.me" : "http://127.0.0.1:8787";
+
 // https://playwright.dev/docs/test-configuration
 const config: PlaywrightTestConfig = {
   use: {
-    baseURL: "http://127.0.0.1:8787",
+    baseURL,
     video: "on",
   },
 };
